13da0b135b37283208fd67dbafc09145cca052b9
MY-APP <hpq833592@gmail.com>
2023-12-10 18:48:26 +0700

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "./component/GlobalStyles";
-import { publicRoutes } from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<GlobalStyles>{<App />}</GlobalStyles>);
