69ef310f1dcc12914c5e96f30875239e5fda4404
MY-APP <hpq833592@gmail.com>
2023-12-10 16:32:26 +0700

    .

---
+++ src/component/Layout/index.js
@@ -0,0 +1,2 @@
+export { default as DefaultLayout } from "./Default/D-index";
+export { default as HeaderOnly } from "./HeaderOnly/H-index";
---
+++ src/pages/Upload/U-index.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Upload = () => {
+  return (
+    <div>
+      <div className="content"></div>
+      <h2>Upload Page</h2>
+    </div>
+  );
+};
+
+export default Upload;
