1ef2c6526704671cc78c278a27207ef570b78996
MY-APP <hpq833592@gmail.com>
2024-01-20 18:46:32 +0700

    .

--- src/component/GlobalStyles/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./GlobalStyles.scss";
-const GlobalStyles = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default GlobalStyles;
--- src/component/Layout/index.js
+++
@@ -1,2 +0,0 @@
-export { default as DefaultLayout } from "./Default/D-index";
-export { default as HeaderOnly } from "./HeaderOnly/H-index";
